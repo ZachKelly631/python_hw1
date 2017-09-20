1d46e54374f1de8b4abaadd834995ecf82efae0c
Sen <zenzosen@gmail.com>
2017-09-19 20:44:35 -0400

    Update P1_number_input.py

--- P1_number_input.py
+++ P1_number_input.py
@@ -1,11 +1,27 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-Author: Sen Varghese
+Authors: Reyes Ceballos and Sen Varghese
 CUS 602
 HW1
-Objective: a guessing game that continues untill responded gives the correct answer
+Objective: a guessing game that continues until responded gives the correct answer
 """
+
+
+# defining the answer of the puzzle
+import random
+H = random.randint(1,9) #Hundreds unit of the answer
+U = random.randint(1,9) #Units unit of the answer
+T = random.randint(max(H,U),9) #Tens unit of the answer
+answer = H*100 + T*10 + U #This the unique generated answer 
+
+#difference between Hundreds and Tens, and Tens and Units of the answer
+diff_ht = T - H
+diff_ut = T - U
+
+#Question 5 does the puzzle you generate have multiple answers?: 
+    #No multiple answers per session
+
 
 #start game with puzzle stored in variable p
 p =  '\n\
@@ -14,13 +30,11 @@ is at least as big as its units digit and \n\
 the tens digit is also at least as big as its hundreds digit.\n\
 '
 
-# defining the answer of the puzzle
-import random
-H = random.randint(1,9) #Hundreds unit of the answer
-U = random.randint(1,9) #Units unit of the answer
-T = random.randint(max(H,U),9) #Tens unit of the answer
-answer = H*100 + T*10 + U #This the unique generated answer 
-#Question 5 does the puzzle you generate have multiple answers?: No multiple answers per session
+#second statement to assist respondend
+p2 = '\n\
+the Tens digit is ' + str(diff_ut) +' greater than Units digit \n\
+and Hundreds digit is ' + str(diff_ht) + ' less than Tens digit.\n\
+'
 
 
 #The main function
@@ -42,19 +56,19 @@ def main():
         if num_str1 < 100 or num_str1 > 999:
             print ('Please enter a 3 digit number\n\
                    ')
-            print(p)
+            print(p2)
             num_str1 = int(input('Please enter an integer: '))
         
         elif num_str1 > answer:
             print('try again, answer is lower')  
-            print(p)
+            print(p2)
             num_str1 = int(input('Please enter an integer: '))
             
         elif num_str1 < answer:
             print('try again, the answer is higher')
-            print(p)
+            print(p2)
             num_str1 = int(input('Please enter an integer: '))
             
     print('You WIN!!')       
-        
+
 main()
